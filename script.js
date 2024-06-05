53e2ff488be2c83191b2b7ceda9cb9d318cca173
Sneha Sailada <snehasailada@gmail.com>
2024-06-05 21:15:46 +0530

    Add files via upload
    Overview:
    This project implements a countdown timer that counts down to a specified date and time. It displays the remaining days, hours, minutes, and seconds until the target date. The timer updates every second, providing a real-time countdown for events or occasions.
    
    Technologies used:
    HTML: Provides the structure of the countdown timer.
    CSS: Styles the countdown timer to make it visually appealing.
    JavaScript: Handles the logic for calculating and updating the countdown timer.
    
    Conclusion:
     It is easily customizable for any event by modifying the target date in the JavaScript file. Ideal for highlighting upcoming events, it ensures accurate and appealing time tracking.

---
+++ script.js
@@ -0,0 +1,30 @@
+// Set the date we're counting down to
+const countDownDate = new Date("June 30, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+const countdownFunction = setInterval(function() {
+
+    // Get today's date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="days", id="hours", id="minutes", and id="seconds"
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+}, 1000);
